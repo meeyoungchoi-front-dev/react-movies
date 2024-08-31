f43084b9a06057f4639b17dbbc78d0911d9a9f79
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-09-01 08:28:29 +0900

    장르정보 매핑해서 가져오기

--- src/common/MovieCard/MovieCard.jsx
+++ src/common/MovieCard/MovieCard.jsx
@@ -6,16 +6,22 @@ import { useMovieGenreQuery } from '../../hooks/useMovieGenre';
 const MovieCard = ({movie}) => {
   const {data: genreData} = useMovieGenreQuery();
 
-
-
-
+  const showGenre = (genereIdList) => {
+    if (!genreData) {
+        return []
+    }
+    const genreNameList = genereIdList.map((id) => {
+      const genreObj = genreData.find((genre) => genre.id === id)
+      return genreObj.name;
+    })
+  }
 
   return (
     <div 
     style={{backgroundImga:"url(" + `https://media.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path}` + ")" }} className="movie-card">
      <div className="overlay">
         <h1>{movie.title}</h1>
-        {movie.genre_ids.map((id) => {
+        {showGenre(movie.genre_ids).map((id) => {
             <Badge bg="secondary">{id}</Badge>
         })}
         <div>
